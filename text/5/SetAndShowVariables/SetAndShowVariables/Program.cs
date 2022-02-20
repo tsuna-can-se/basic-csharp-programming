ea765edef0ca6a49db34ae4c3300d175f3ee8b54
tsuna-can <61451753+tsuna-can-se@users.noreply.github.com>
2022-02-20 23:22:15 +0900

    変数のサンプルPGを追加

---
+++ text/5/SetAndShowVariables/SetAndShowVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SetAndShowVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 変数に値を代入する。
+            int age = 30;
+            string name = "ツナ缶";
+
+            // 変数の値を表示する。
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+        }
+    }
+}
