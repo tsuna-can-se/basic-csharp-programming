aa385cc5a2cafb6342682cfab820b13d0dafc421
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-02-12 17:18:15 +0900

    出力する値を追記

--- text/07/ArrayVariables/Program.cs
+++ text/07/ArrayVariables/Program.cs
@@ -4,6 +4,6 @@ items[0] = 1;
 items[1] = 2;
 items[2] = 3;
 
-Console.WriteLine(items[0]);
-Console.WriteLine(items[1]);
-Console.WriteLine(items[2]);
+Console.WriteLine(items[0]); //  output: 1
+Console.WriteLine(items[1]); //  output: 2
+Console.WriteLine(items[2]); //  output: 3
