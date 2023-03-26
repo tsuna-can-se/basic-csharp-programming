56f32745ba3ed38d9b79e7dc6ea18b734d389f16
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-03-26 12:35:56 +0900

    whileのサンプルを追加

---
+++ text/08/WhileStatement/Program.cs
@@ -0,0 +1,8 @@
+﻿int rand = Random.Shared.Next(0, 5);
+while (rand != 0)
+{
+    Console.WriteLine("乱数は" + rand + "でした。");
+    rand = Random.Shared.Next(0, 5);
+}
+
+Console.WriteLine("whileを終了します。");
