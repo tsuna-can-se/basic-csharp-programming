86e264e9fc3d2ba871f5598c725c11698273a9b4
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-05-21 09:40:06 +0900

    9章のサンプルを追加

---
+++ text/09/ShoppingSample/ProductShelf.cs
@@ -0,0 +1,20 @@
+﻿namespace ShoppingSample;
+
+public class ProductShelf
+{
+    private List<Product> products = new List<Product>();
+
+    public Product? this[string productName]
+    {
+        get
+        {
+            return this.products
+                .Find(p => p.Name == productName);
+        }
+    }
+
+    public void Stock(List<Product> products)
+    {
+        this.products.AddRange(products);
+    }
+}
---
+++ text/09/ShoppingSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingSample;
+
+Product candy = new Product() { Name = "あめ", UnitPrice = 10m };
+Product chewingGum = new Product() { Name = "ガム", UnitPrice = 20m };
+Product cookie = new Product() { Name = "クッキー", UnitPrice = 50m };
+Product chocolate = new Product() { Name = "チョコ", UnitPrice = 40m };
+ProductShelf shelf = new ProductShelf();
+shelf.Stock(new List<Product>() { candy, chewingGum, cookie, chocolate });
+
+List<string> shoppingList = new List<string>()
+{
+    "あめ,3",
+    "クッキー,2",
+    "せんべい,1",
+};
+Customer customer1 = new Customer(shoppingList, shelf, 200);
+customer1.Shopping();
