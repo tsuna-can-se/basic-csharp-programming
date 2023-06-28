3550f93138003a7be6366c89b686ddb89e7e0665
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-06-29 01:13:18 +0900

    ポリモーフィズムのサンプルを追加

---
+++ text/12/PolymorphismSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using PolymorphismSample;
+
+Console.WriteLine("===具象クラスを利用する例===");
+Animal animal = new Cat();
+animal.Walk();
+
+Console.WriteLine("===抽象クラスを利用する例===");
+string loggerType = "FlatText";
+
+DoSomething(loggerType);
+
+static void DoSomething(string loggerType)
+{
+    ConsoleLoggerBase consoleLogger = CreateConsoleLogger(loggerType);
+    consoleLogger.WriteLog("ログテキスト");
+}
+
+static ConsoleLoggerBase CreateConsoleLogger(string loggerType)
+{
+    switch (loggerType)
+    {
+        case "FlatText":
+            return new FlatTextConsoleLogger();
+        case "Json":
+            return new JsonConsoleLogger();
+        default:
+            throw new NotSupportedException();
+    }
+}
+
