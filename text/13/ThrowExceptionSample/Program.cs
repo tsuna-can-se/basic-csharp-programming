349129eff49fd6a85d45f4d0d8e4bbf5eb132b34
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-07-01 18:21:12 +0900

    例外を発生させるサンプルを追加

---
+++ text/13/ThrowExceptionSample/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("文字列を入力してください。");
+string? arg = Console.ReadLine();
+if (string.IsNullOrEmpty(arg))
+{
+    string message = "空の文字列は入力できません。";
+    throw new ArgumentException(message);
+}
+
+Console.WriteLine("入力値:" + arg);
