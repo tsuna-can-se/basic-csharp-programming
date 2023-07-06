8d313d5b76c9a8643124f2ce3091022bc53af297
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-07-06 23:39:41 +0900

    演習14-02を追加

---
+++ exercise/14/Ex14_02/Program.cs
@@ -0,0 +1,10 @@
+﻿using Ex14_02;
+
+int baseValue = 2;
+int exponent1 = 0;
+int exponent2 = 1;
+int exponent3 = 5;
+
+Console.WriteLine("2^0 = " + baseValue.Power(exponent1));
+Console.WriteLine("2^1 = " + baseValue.Power(exponent2));
+Console.WriteLine("2^5 = " + baseValue.Power(exponent3));
