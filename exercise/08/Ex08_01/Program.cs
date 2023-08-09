ed2e8c4fb84cbbf355329927534a0da8d7158a50
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2023-08-10 00:21:23 +0900

    演習08-01の文字列補完を修正

--- exercise/08/Ex08_01/Program.cs
+++ exercise/08/Ex08_01/Program.cs
@@ -4,13 +4,13 @@ var year = Convert.ToInt32(input);
 
 if (year % 4 == 0 && year % 100 != 0)
 {
-    Console.WriteLine($"{year}年はうるう年です。");
+    Console.WriteLine(year + "年はうるう年です。");
 }
 else if (year % 400 == 0)
 {
-    Console.WriteLine($"{year}年はうるう年です。");
+    Console.WriteLine(year + "年はうるう年です。");
 }
 else
 {
-    Console.WriteLine($"{year}年は平年です。");
+    Console.WriteLine(year + "年は平年です。");
 }
