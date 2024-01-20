06cc572e067880f995067cd9606d725eaacad582
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-01-20 16:37:13 +0900

    総合演習の回答を追加

---
+++ exercise/17/Ex17/DecimalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Ex17;
+
+public static class DecimalExtensions
+{
+    public static string ToJPNYen(this decimal value)
+    {
+        return value.ToString("C", new CultureInfo("ja-JP"));
+    }
+}
