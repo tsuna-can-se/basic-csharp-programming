32bcb7132b320240fc6bf2504af50e1ea73ff357
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-02-26 00:56:03 +0900

    await忘れのサンプルを追加

--- text/17/TaskSample/Program.cs
+++ text/17/TaskSample/Program.cs
@@ -1,13 +1,13 @@
 ﻿using TaskSample;
 
-//await new MultipleAwait1().DoSomethingAsync();
-//Console.WriteLine("==========================");
+await new MultipleAwait1().DoSomethingAsync();
+Console.WriteLine("==========================");
 
-//await new MultipleAwait2().DoSomethingAsync();
-//Console.WriteLine("==========================");
+await new MultipleAwait2().DoSomethingAsync();
+Console.WriteLine("==========================");
 
-//await new MultipleAwait3().DoSomethingAsync();
-//Console.WriteLine("==========================");
+await new MultipleAwait3().DoSomethingAsync();
+Console.WriteLine("==========================");
 
 await new TaskT1().DoSomethingAsync1();
 Console.WriteLine("==========================");
@@ -16,4 +16,7 @@ await new TaskT1().DoSomethingAsync2();
 Console.WriteLine("==========================");
 
 new TaskT1().DoSomething();
+Console.WriteLine("==========================");
+
+new ForgetAwait().DoSomething();
 Console.WriteLine("==========================");
