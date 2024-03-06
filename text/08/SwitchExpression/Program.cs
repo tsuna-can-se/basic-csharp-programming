1842ead1ab7481ce935ee0a39c437fd5c6cbffd3
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-03-06 12:47:57 +0900

    基本文法のサンプルを追加

---
+++ text/08/SwitchExpression/Program.cs
@@ -0,0 +1,13 @@
+﻿DateTime now = DateTime.Now;
+var dayOfWeek = now.DayOfWeek switch
+{
+    DayOfWeek.Monday => "月曜日",
+    DayOfWeek.Tuesday => "火曜日",
+    DayOfWeek.Wednesday => "水曜日",
+    DayOfWeek.Thursday => "木曜日",
+    DayOfWeek.Friday => "金曜日",
+    DayOfWeek.Saturday => "土曜日",
+    DayOfWeek.Sunday => "日曜日",
+    _ => "Unknown"
+};
+Console.WriteLine(dayOfWeek);
